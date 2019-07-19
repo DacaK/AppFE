a2b99760886ead31bea43b0e739ded26cf55833d
Danijela Krstic <danijela.krstic@eng.it>
2019-07-19 15:18:03 +0200

    deleted unnecessary files

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,7 +1,7 @@
+import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { NgModule } from '@angular/core';
 
 const pagesRoutes: Routes = [
     { path: 'dashboard', component: DashboardComponent },
